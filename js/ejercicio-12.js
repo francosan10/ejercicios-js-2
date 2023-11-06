eba2e7fdc0fc3af8a5d8cb0fab25480fb4efe7ec
Franco <lmassa.fms@gmail.com>
2023-11-06 15:52:11 -0300

    agrego ejercicio 12

---
+++ js/ejercicio-12.js
@@ -0,0 +1,5 @@
+// 12- Realiza un script que genere un número aleatorio entre 1 y 99.
+
+let numeroRandom = Math.floor(Math.random() * 99) + 1;
+
+document.write (`Numero aleatoririo entre 1 y 99: ${numeroRandom}`);
