52a5cbecc094072b2b24ade692f068c452cf26f2
Franco <lmassa.fms@gmail.com>
2023-11-08 13:35:39 -0300

    agrego ejercicio13

---
+++ js/ejercicio-13.js
@@ -0,0 +1,5 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+const texto = prompt('Ingrese texto a convertit');
+
+const textoMayus = document.write(texto.toUpperCase());
